da78bf615c11e0e71cfc755406d901d8367cf63b
Intetsu Paris <intetsuoff@gmail.com>
2024-01-23 03:42:46 +0300

    api changed

--- src/components/Home/Trending/TrendingFilm.jsx
+++ src/components/Home/Trending/TrendingFilm.jsx
@@ -11,11 +11,16 @@ import Fire from './img/Fire.svg'
 function TrendingFilm() {
     const [movieList, setMovieList] = useState([]);
 
-  const getMovies=()=>{
-    fetch('https://api.themoviedb.org/3/discover/movie?api_key=cb46d76a0b00b19847f93f36a4873953')
-    .then(response => response.json())
-   .then(json=>setMovieList(json.results))
-  }
+    const getMovies = async () => {
+      try {
+        const response = await fetch('https://api.themoviedb.org/3/discover/movie?api_key=cb46d76a0b00b19847f93f36a4873953');
+        const json = await response.json();
+        setMovieList(json.results);
+      } catch (error) {
+        console.error('Error fetching movies:', error);
+      }
+    };
+    
 
   useEffect(()=> {
 getMovies()
