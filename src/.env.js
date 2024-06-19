4378b9d710e31afe13c618cb8f6d21e8bed48fdb
IntetsuParis <97919071+IntetsuParis@users.noreply.github.com>
2024-06-19 20:48:54 +0300

    123

--- src/.env.js
+++
@@ -1,4 +0,0 @@
-export const API_KEY =
-  "9Qv5_4hEumJ7ER9IdikB6icbZ9Hrvfxoeipyn4BG3f51jHMN0uQe.NAMw43wATI";
-export const SECRET_API_KEY =
-  "cFhD7QFsCMh6h+bWTUbORenHf7tpkZ5OmWGJEGNmp6kC+US0rPUfEhopYtF7GTYG94avtDcNv0Y9t6nLgMMX6oP62uhP94Tn+e6gizXz1gizUMOnth6ozK36HtSbzUw0";
